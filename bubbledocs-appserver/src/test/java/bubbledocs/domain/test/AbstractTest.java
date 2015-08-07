79299915cc6fac7985672059f089b90190bc5f3c
Pedro Fialho <pedro.b.fialho@ist.utl.pt>
2015-08-07 23:50:02 +0100

    [UPD] Added abstract class test for test setup and teardown

---
+++ bubbledocs-appserver/src/test/java/bubbledocs/domain/test/AbstractTest.java
@@ -0,0 +1,35 @@
+package bubbledocs.domain.test;
+
+import javax.transaction.NotSupportedException;
+import javax.transaction.SystemException;
+
+import org.junit.After;
+import org.junit.Before;
+
+import pt.ist.fenixframework.FenixFramework;
+import pt.ist.fenixframework.core.WriteOnReadError;
+
+public abstract class AbstractTest {
+	
+    @Before
+    public void setUp() throws Exception {
+        try {
+            FenixFramework.getTransactionManager().begin(false);
+            initializeDomain();
+        } catch (WriteOnReadError | NotSupportedException | SystemException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @After
+    public void tearDown() {
+        try {
+            FenixFramework.getTransactionManager().rollback();
+        } catch (IllegalStateException | SecurityException | SystemException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected abstract void initializeDomain();
+
+}
